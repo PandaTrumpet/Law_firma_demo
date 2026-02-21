299465121aa2b531d68927f806a16cb303f47c78
PandaTrumpet <dsavontrumpet@gmail.com>
2026-02-21 21:39:14 +0100

    update redurect

--- my-app/middleware.ts
+++ my-app/middleware.ts
@@ -1,9 +1,22 @@
+// import createMiddleware from "next-intl/middleware";
+
+// export default createMiddleware({
+//   locales: ["en", "he", "ru"],
+//   defaultLocale: "en",
+//   localePrefix: "always",
+// });
+
+// export const config = {
+//   matcher: ["/((?!api|_next|.*\\..*).*)"],
+// };
+
 import createMiddleware from "next-intl/middleware";
 
 export default createMiddleware({
   locales: ["en", "he", "ru"],
   defaultLocale: "en",
   localePrefix: "always",
+  localeDetection: false, // ✅ ВАЖНО
 });
 
 export const config = {
